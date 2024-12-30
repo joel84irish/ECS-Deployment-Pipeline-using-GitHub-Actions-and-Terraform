c970b9dd25d9dd0e563593933031e2fc1639afac
joel84irish <125594109+joel84irish@users.noreply.github.com>
2024-12-30 17:59:47 +0000

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,9 @@
 # provider.tf
 
-# Specify the provider and access details
 provider "aws" {
-  profile = "default"
   region  = terraform.workspace
   default_tags {
     tags = {
       Environment = terraform.workspace
     }
   }
-}
