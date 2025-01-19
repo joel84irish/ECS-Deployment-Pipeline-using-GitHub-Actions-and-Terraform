eb7913e39db8da7e1b0b6081d74858b819c905d4
joel84irish <125594109+joel84irish@users.noreply.github.com>
2025-01-19 19:12:53 +0000

    Update outputs.tf

--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -6,10 +6,6 @@ output "public_subnet_ids" {
   value = aws_subnet.public[*].id
 }
 
-output "nat_gateway_ids" {
-  value = aws_nat_gateway.gw[*].id
-}
-
 output "vpc_id" {
   value = aws_vpc.main.id
 }
