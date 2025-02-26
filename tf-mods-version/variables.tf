307b1d87590347d4ab80ba82281203b31cb92b18
joel84irish <125594109+joel84irish@users.noreply.github.com>
2025-02-26 12:18:25 +0000

    Update variables.tf

--- tf-mods-version/variables.tf
+++ tf-mods-version/variables.tf
@@ -10,20 +10,15 @@ variable "domain_name" {
   type        = string
 }
 
-variable "subdomain" {
-  default     = "tm"
-  description = "The subdomain to use for the project"
-  type        = string
-}
 
 variable "ecr_repo_url" {
   description = "The URL of the ECR repository"
-  default     = "010928194025.dkr.ecr.eu-west-2.amazonaws.com/tm"
+  default     = "980921749029.dkr.ecr.us-west-2.amazonaws.com/threat-model"
   type        = string
 }
 
 variable "aws_region" {
-  default     = "eu-west-2"
+  default     = "us-west-2"
   description = "The AWS region to deploy to"
   type        = string
 }
