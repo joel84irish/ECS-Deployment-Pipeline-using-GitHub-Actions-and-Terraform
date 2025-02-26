afea6b053aefb18bfdbbe5ea430d6d1bae4e0260
joel84irish <125594109+joel84irish@users.noreply.github.com>
2025-02-26 11:31:52 +0000

    Update providers.tf

--- tf-mods-version/providers.tf
+++ tf-mods-version/providers.tf
@@ -11,7 +11,7 @@ terraform {
   backend "s3" {
     bucket  = "joel-tm"
     key     = "joel-tm/terraform.tfstate"
-    region  = "eu-west-2"
+    region  = "us-west-2"
     encrypt = true
     # dynamodb_table = "your-terraform-lock-table"
   }
